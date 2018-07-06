06674933f347d283d162ab14b2e98370198d969a
Albert16061 <40551377+Albert16061@users.noreply.github.com>
2018-07-07 02:36:26 +0700

    Index V1.0
    Get info Successful
    have banner
    navbar ok
    breadcumb ok

--- Project_III_Dr4rum/Project_III_Dr4rum/App_Start/BundleConfig.cs
+++ Project_III_Dr4rum/Project_III_Dr4rum/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace Project_III_Dr4rum
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/index.css"));
         }
     }
 }
---
+++ Project_III_Dr4rum/Project_III_Dr4rum/Controllers/IndexController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Project_III_Dr4rum.Models;
+
+namespace Project_III_Dr4rum.Controllers
+{
+    public class IndexController : Controller
+    {
+        Dr4rumEntities db = new Dr4rumEntities();
+        // GET: Index
+        public ActionResult Index()
+        {
+            var listGroup = db.Groups.ToList();
+            return View(listGroup);
+        }
+    }
+}
